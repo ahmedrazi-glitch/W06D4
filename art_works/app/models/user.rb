e6efe7a8db995c3537e839111837a07f6f291f07
Elvin Atwine <elivnatwiine@gmail.com>
2023-03-09 12:10:06 -0800

    switch

---
+++ art_works/app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    has_many :artworks,
+    foreign_key: :artist_id,
+    inverse_of: :artist
+    
+    validates :name, presence: true
+
+end
